fddc7753570d3a1ecc0a9ca3616ad85dc4892a95
Aaron Hurley <aaronshurley@gmail.com>
2018-09-15 12:34:10 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# dotfiles
+Setup for a new Mac. Largely inspired by [mathiasbyens](https://github.com/mathiasbynens/dotfiles).
+
+## Install
+```bash
+./bootstrap.sh
+```
+
+## Update MacOS Preferences
+This is a one-time setup and doesn't run as part of `bootstrap.sh`. Restart machine for some changes to take effect.
+```bash
+./.macos
+```
+
+## Add Tools or Apps
+Using [Homebrew](https://brew.sh/), edit `Brewfile` and then `./bootstrap.sh`.
+
+## Patterns
+- idempotent install
+- Use brew to install as much as possible
+- nvim with [luan's vimfiles](https://github.com/luan/vimfiles)
---
+++ bootstrap.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+git pull origin master
+
+function brew_all_the_things() {
+  if [[ -z "$(command -v brew)" ]]; then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  echo "Running the Brewfile..."
+  brew update
+  brew upgrade
+  brew tap Homebrew/bundle
+  ln -sf "$(pwd)/Brewfile" "${HOME}/.Brewfile"
+  brew bundle --global
+  brew bundle cleanup
+}
+
+function setup_vim() {
+  if [[ -d "${HOME}/.vim" ]]; then
+    echo "removing ~/.vim dir"
+    rm -rf "${HOME}/*.vim"
+  else
+    echo "Installing luan's vimfiles..."
+    curl vimfiles.luan.sh/install | FORCE=1 bash
+  fi
+
+  echo "Updating vim..."
+  vim-update
+}
+
+function rsync_and_source() {
+  rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".macos" \
+    --exclude "bootstrap.sh" \
+    -avh --no-perms . ~
+  source ~/.bash_profile
+}
+
+function do_it() {
+  brew_all_the_things
+  setup_vim
+  rsync_and_source
+}
+
+function main() {
+  if [[ "$1" == "-f" ]]; then
+    do_it
+  else
+    read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
+      do_it
+    fi
+  fi
+}
+
+main
